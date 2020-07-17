1fdbabea1597293e812f1e07e3cdffe181cf5971
homolka <n.homolka@dkfz.de>
2020-07-17 17:26:08 +0200

    edited density modulation function to be in line with matRad naming

---
+++ heterogeneity/densityModulation/matRad_modulateDensity.m
@@ -0,0 +1,79 @@
+function [ct]=matRad_modulateDensity(ct,cst,Pmod)
+% matRad density modulation function
+% 
+% call
+%   ct = matRad_modulateDensity(ct,cst,Pmod)
+%
+% input
+%   ct:             ct struct
+%   cst:            matRad cst struct
+%   Pmod:           Modulation power according to which the modulation will
+%                   be created
+%
+% output
+%   ct:             ct struct with modulated density cube
+%
+% References
+%   [1] https://iopscience.iop.org/article/10.1088/1361-6560/aa641f
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Copyright 2020 the matRad development team. 
+% 
+% This file is part of the matRad project. It is subject to the license 
+% terms in the LICENSE file found in the top-level directory of this 
+% distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part 
+% of the matRad project, including this file, may be copied, modified, 
+% propagated, or distributed except according to the terms contained in the 
+% LICENSE file.
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+global matRad_cfg;
+matRad_cfg =  MatRad_Config.instance();
+
+
+DensMod = loadModDist(Pmod);
+
+DensMod(1,1)=0.001;
+DensMod(:,4)=DensMod(:,2);
+
+%Connect each density-probability-couple with a number that will later be transformed the HU-Value:
+%The maximum HU-Value of the HU set by Schneider etal is 2995: So the HU of the modulated density must be at least 2995+1=2996 ; This values is prerocessed with the later used RescaleIntercept and RescaleSlope. See also calculation for Threshold_HU_Value_to_double.
+Min_HU_for_DensMod_in_double=((2995+1000)/1);
+
+%HU as double in row 2:
+for i=1:length(DensMod)
+    DensMod(i,2)=Min_HU_for_DensMod_in_double+i; %For i=1 the HU value is then 2996, the corresponding double is 3996
+end
+%Real HU values in row 3:
+for i=1:length(DensMod)
+    DensMod(i,3)=DensMod(i,2)*1-1000; %For i=1 the HU value is then 2996, the corresponding double is 3996
+end
+
+
+idx = contains(cst(:,2),'Lung');
+if sum(idx)==0
+    matRad_cfg.dispError('No lung segmentation found in cst.\n');    
+end
+
+lungIdx = [cst{idx,4}];
+lungIdx = unique([lungIdx{:}]);
+
+Z_mod = ct.cube{1};
+
+numOfSamples = 50*length(lungIdx);
+
+zz1 = ceil(rand(numOfSamples,1)*length(DensMod));
+zz2 = rand(numOfSamples,1);
+ix = zz2 <= DensMod(zz1,4);
+newDist = zz1(ix);
+Z_mod(lungIdx) = DensMod(newDist(lungIdx),2);
+
+figure, histogram(Z_mod(lungIdx))
+
+ct.cube{1} = Z_mod;
+
+%createSchneiderConverter(DensMod);
+
+end

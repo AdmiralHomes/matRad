cf1f5427d0e1a9d654b32120680a9fa7b4c95f04
homolka <n.homolka@dkfz.de>
2020-07-22 16:00:29 +0200

    simplified modulateDensity function to be more efficient and faster

--- heterogeneity/densityModulation/matRad_modulateDensity.m
+++ heterogeneity/densityModulation/matRad_modulateDensity.m
@@ -32,11 +32,13 @@ function [ct]=matRad_modulateDensity(ct,cst,Pmod)
 global matRad_cfg;
 matRad_cfg =  MatRad_Config.instance();
 
-
 DensMod = loadModDist(Pmod);
 
 DensMod(1,1)=0.001;
 DensMod(:,4)=DensMod(:,2);
+
+%normalize density distribution
+DensMod(:,4) = DensMod(:,4) / sum(DensMod(:,4));
 
 %Connect each density-probability-couple with a number that will later be transformed the HU-Value:
 %The maximum HU-Value of the HU set by Schneider etal is 2995: So the HU of the modulated density must be at least 2995+1=2996 ; This values is prerocessed with the later used RescaleIntercept and RescaleSlope. See also calculation for Threshold_HU_Value_to_double.
@@ -51,29 +53,19 @@ for i=1:length(DensMod)
     DensMod(i,3)=DensMod(i,2)*1-1000; %For i=1 the HU value is then 2996, the corresponding double is 3996
 end
 
-
+% get all unique lung indices from lung segmentations
 idx = contains(cst(:,2),'Lung');
 if sum(idx)==0
     matRad_cfg.dispError('No lung segmentation found in cst.\n');    
 end
-
 lungIdx = [cst{idx,4}];
 lungIdx = unique([lungIdx{:}]);
 
-Z_mod = ct.cube{1};
+% descrete sampling of the density distribution
+P = [0; cumsum(DensMod(:,4))];
+ct.cube{1}(lungIdx) = discretize(rand(numel(lungIdx),1),P);
 
-numOfSamples = 50*length(lungIdx);
-
-zz1 = ceil(rand(numOfSamples,1)*length(DensMod));
-zz2 = rand(numOfSamples,1);
-ix = zz2 <= DensMod(zz1,4);
-newDist = zz1(ix);
-Z_mod(lungIdx) = DensMod(newDist(lungIdx),2);
-
-figure, histogram(Z_mod(lungIdx))
-
-ct.cube{1} = Z_mod;
-
-%createSchneiderConverter(DensMod);
+% plot histogram of the the lung density distribution
+figure, histogram(ct.cube{1}(lungIdx))
 
 end

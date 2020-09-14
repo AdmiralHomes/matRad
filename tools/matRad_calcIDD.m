d583df0aa30e951d6c5ead5ae802ed7c8835cea6
homolka <n.homolka@dkfz.de>
2020-09-14 19:54:23 +0200

    added matRad descriptions to calcIDD and new StfPencilBeam functions

--- tools/matRad_calcIDD.m
+++ tools/matRad_calcIDD.m
@@ -1,6 +1,29 @@
 function IDD = matRad_calcIDD(doseCube,direction)
-%MATRAD_IDD Summary of this function goes here
-%   Detailed explanation goes here
+% function to calculate integrated depth dose (IDD) of a simple boxphantom
+% for beams in x-, y- or z-direction
+% 
+% call
+%   IDD = matRad_calcIDD(doseCube,direction)
+%
+% input
+%   doseCube:       calculated dose cube
+%   direction:      'x','y' or 'z', direction of the beam
+%
+% output
+%   IDD:            depth dose vector 
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Copyright 2020 the matRad development team. 
+% 
+% This file is part of the matRad project. It is subject to the license 
+% terms in the LICENSE file found in the top-level directory of this 
+% distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part 
+% of the matRad project, including this file, may be copied, modified, 
+% propagated, or distributed except according to the terms contained in the 
+% LICENSE file.
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if nargin < 2
     direction = 'y';
 end
--- tools/matRad_generateStfPencilBeam.m
+++ tools/matRad_generateStfPencilBeam.m
@@ -1,11 +1,34 @@
-function stf = matRad_generateStfPencilBeam(pln,EnergyIx)
-
+function stf = matRad_generateStfPencilBeam(pln,energyIx)
+% function to generate very basic stf file for a single energy
+% 
+% call
+%   stf = matRad_generateStfPencilBeam(pln,EnergyIx)
+%
+% input
+%   pln:            matRads planning struct
+%   energyIx:       index of desired energy of the machine data
+%
+% output
+%   stf:            matRads steering struct
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Copyright 2020 the matRad development team. 
+% 
+% This file is part of the matRad project. It is subject to the license 
+% terms in the LICENSE file found in the top-level directory of this 
+% distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part 
+% of the matRad project, including this file, may be copied, modified, 
+% propagated, or distributed except according to the terms contained in the 
+% LICENSE file.
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 load([pln.radiationMode,'_',pln.machine]);
 
 SAD = machine.meta.SAD;
 currentMinimumFWHM = matRad_interp1(machine.meta.LUT_bxWidthminFWHM(1,:)', machine.meta.LUT_bxWidthminFWHM(2,:)',pln.propStf.bixelWidth);
 
-%% generate stf parameters
+% generate stf parameters
 stf.gantryAngle = pln.propStf.gantryAngles;
 stf.couchAngle = pln.propStf.couchAngles;
 stf.bixelWidth = pln.propStf.bixelWidth;
@@ -20,9 +43,9 @@ stf.totalNumOfBixels = 1;
 stf.soucePoint_bev = [0,-SAD,0];
 stf.soucePoint = [0,-SAD,0];
 
-%% generate ray
-stf.ray.energy = machine.data(EnergyIx).energy;
-stf.ray.focusIx = find(machine.data(EnergyIx).initFocus.SisFWHMAtIso > currentMinimumFWHM,1,'first');
+% generate ray
+stf.ray.energy = machine.data(energyIx).energy;
+stf.ray.focusIx = find(machine.data(energyIx).initFocus.SisFWHMAtIso > currentMinimumFWHM,1,'first');
 stf.ray.rangeShifter = struct;
 stf.ray.rangeShifter.ID = 0;
 stf.ray.rangeShifter.eqThickness = 0;
